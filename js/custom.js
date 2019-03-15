3ad0565f803061ccadc0c218f5c5d930015e1540
muga-mark <markmuga01@gmail.com>
2019-03-15 19:23:04 +0800

    okay na ang modal. POTANG INA. haha, content nalng ang kuwang sa modal sa work history

--- js/custom.js
+++ js/custom.js
@@ -104,9 +104,26 @@ $(document).ready(function () {
     });
 
     //animatedModal
-    $("#demo01,#demo02,#demo03,#demo04,#demo05,#demo06,#demo07,#demo08,#demo09").animatedModal();
+    $("#work1").animatedModal({modalTarget: 'history1',animatedIn: 'lightSpeedIn', animatedOut: 'bounceOutDown'});
+    $("#work2").animatedModal({modalTarget: 'history2',animatedIn: 'lightSpeedIn', animatedOut: 'bounceOutDown'});
+    $("#work3").animatedModal({modalTarget: 'history3',animatedIn: 'lightSpeedIn', animatedOut: 'bounceOutDown'});
+    $("#work4").animatedModal({modalTarget: 'history4',animatedIn: 'lightSpeedIn', animatedOut: 'bounceOutDown'});
+    $("#work5").animatedModal({modalTarget: 'history5',animatedIn: 'lightSpeedIn', animatedOut: 'bounceOutDown'});
+    $("#work6").animatedModal({modalTarget: 'history6',animatedIn: 'lightSpeedIn', animatedOut: 'bounceOutDown'});
+    $("#work7").animatedModal({modalTarget: 'history7',animatedIn: 'lightSpeedIn', animatedOut: 'bounceOutDown'});
+    $("#work8").animatedModal({modalTarget: 'history8',animatedIn: 'lightSpeedIn', animatedOut: 'bounceOutDown'});
+    $("#work9").animatedModal({modalTarget: 'history9',animatedIn: 'lightSpeedIn', animatedOut: 'bounceOutDown'});
+    $("#work10").animatedModal({modalTarget: 'history10',animatedIn: 'lightSpeedIn', animatedOut: 'bounceOutDown'});
+    $("#work11").animatedModal({modalTarget: 'history11',animatedIn: 'lightSpeedIn', animatedOut: 'bounceOutDown'});
+    $("#work12").animatedModal({modalTarget: 'history12',animatedIn: 'lightSpeedIn', animatedOut: 'bounceOutDown'});
+    $("#work13").animatedModal({modalTarget: 'history13',animatedIn: 'lightSpeedIn', animatedOut: 'bounceOutDown'});
+    $("#work14").animatedModal({modalTarget: 'history14',animatedIn: 'lightSpeedIn', animatedOut: 'bounceOutDown'});
+    $("#work15").animatedModal({modalTarget: 'history15',animatedIn: 'lightSpeedIn', animatedOut: 'bounceOutDown'});
+    $("#work16").animatedModal({modalTarget: 'history16',animatedIn: 'lightSpeedIn', animatedOut: 'bounceOutDown'});
+    $("#work17").animatedModal({modalTarget: 'history17',animatedIn: 'lightSpeedIn', animatedOut: 'bounceOutDown'});
+    $("#work18").animatedModal({modalTarget: 'history18',animatedIn: 'lightSpeedIn', animatedOut: 'bounceOutDown'});
 
-    // Contact Form 	
+    // Contact Form 	        
 
     // validate contact form
     $(function () {
