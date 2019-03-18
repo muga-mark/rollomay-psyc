ce818b094d830ff566d8ea4bbdd67f7a4eda62f3
nekewl <christiinenicoledelossantos@gmail.com>
2019-03-18 21:04:20 +0800

    barney and friends added

--- js/custom.js
+++ js/custom.js
@@ -180,3 +180,14 @@ $(document).ready(function () {
 
     });
 });
+
+
+
+//Friends
+$('.panel-collapse').on('show.bs.collapse', function () {
+    $(this).siblings('.panel-heading').addClass('active');
+  });
+
+  $('.panel-collapse').on('hide.bs.collapse', function () {
+    $(this).siblings('.panel-heading').removeClass('active');
+  });
